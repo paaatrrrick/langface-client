abd829d1f32a33a6a2b62d053fafd5457e5e35c7
patrick.123.foster@gmail.com <patrick.123.foster@gmail.com>
2023-08-04 22:44:14 -0700

    vid back

--- src/components/launch/components/heroOne.tsx
+++ src/components/launch/components/heroOne.tsx
@@ -16,8 +16,11 @@ const HeroOne : React.FC<HeroOneProps> = ({demoClick}) => {
                                 <h1 className="font-semibold text-mainDark text-3xl md:text-4xl" style={{lineHeight: '3.25rem'}}>Run Blog Marketing on Autopilot</h1>
                                 <p className="mt-15px text-xl text-mainDark">Want to start a blog to drive organic traffic to your website? Hire an AI agent that works autonomously to help with all aspects of blog growth!</p>
                                 <div className="mt-15px flex items-center justify-center gap-x-6">
-                                    <button onClick={demoClick} className="rounded-full w-60 h-10 bg-brandColor px-3.5 text-m text-center font-sm text-white">Try 3 free demo posts</button>
+                                    <button onClick={demoClick} className="rounded-full py-2 bg-brandColor px-6 text-m text-center font-sm text-white">Try 3 free posts</button>
+                                    <a href="https://www.youtube.com/watch?v=qV-Qc_tDBfc" target="_blank" className="flex align-middle py-2 px-10 justify-center rounded-full bg-transparent border-brandColor border border-solid text-m font-sm text-brandColor text-center">Watch Video <span className="ml-3">▶</span></a>
                                 </div>
+                                {/* <div className="mt-15px flex items-center justify-center gap-x-6">
+                                </div> */}
                             </div>
                             {/* <div className="mt-15px flex items-center justify-center gap-x-6">
                                 <iframe width="420" height="315" src="https://www.youtube.com/embed/qV-Qc_tDBfc"></iframe>
